4e1d48a82591ad917a2701827161f6077f22b6a2
hidden bebb <cbj4133@naver.com>
2023-09-25 22:17:54 +0900

    Changes to be committed: 	modified:   __pycache__/db_connect.cpython-311.pyc 	modified:   app.py 	modified:   db_connect.py

--- app.py
+++ app.py
@@ -9,9 +9,15 @@ app.register_blueprint(board)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:12345678@localhost:3306/a"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+app.config['SECRET_KEY'] ="Secret key"
 
 db.init_app(app)
 bcrypt = Bcrypt(app)
 
+def create_tables():
+    with app.app_context():
+        db.create_all()
+
 if __name__ == '__main__':
+    create_tables()
     app.run(debug=True)
